d0835667ecc7b9c0ca746533c29fb9c81356aade
Nikhil Verma <nikhilgotmail@gmail.com>
2023-03-07 14:53:36 +0530

    Updates

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    overrides: [
+        {
+            files: ["package.json", "*.html"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
